2bf527fc84645a683489e875294cf56266f5dede
barklan <buzin.gb@phystech.edu>
2021-01-26 01:32:09 +0300

    df

--- core_package/utils.py
+++ core_package/utils.py
@@ -1,3 +1,11 @@
 def f(a, b):
-    """Some docstring"""
+    """Returns the sum of two elements.
+
+    Args:
+        a (Any): var1
+        b (Any): var2
+
+    Returns:
+        Any: output
+    """
     return a + b
